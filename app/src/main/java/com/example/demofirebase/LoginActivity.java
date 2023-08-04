15402b2f413c242b4a0b94958b000edcd3b4e798
Thanhtam2909 <tamntph23613@fpt.edu.vn>
2023-08-04 18:36:13 +0700

    first commit

--- app/src/main/java/com/example/demofirebase/LoginActivity.java
+++ app/src/main/java/com/example/demofirebase/LoginActivity.java
@@ -1,13 +1,85 @@
 package com.example.demofirebase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edt_Email, edt_Password;
+    private Button btn_Login, btn_SingUp;
+    TextView status;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+
+        edt_Email = (EditText) findViewById(R.id.edt_email);
+        edt_Password = (EditText) findViewById(R.id.edt_pass);
+        btn_Login = (Button) findViewById(R.id.btn_login);
+        btn_SingUp = (Button) findViewById(R.id.btn_singup);
+        status = (TextView) findViewById(R.id.status);
+
+        btn_SingUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mail, pass;
+                mail = edt_Email.getText().toString();
+                pass = edt_Password.getText().toString();
+
+                if (TextUtils.isEmpty(mail)) {
+                    Toast.makeText(LoginActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(pass)){
+                    Toast.makeText(LoginActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.signInWithEmailAndPassword(mail, pass)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(LoginActivity.this, "Login thành công.",
+                                            Toast.LENGTH_SHORT).show();
+                                    status.setText("Login thanh cong");
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(LoginActivity.this, "Đăng nhập không thành công",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/demofirebase/MainActivity.java
+++ app/src/main/java/com/example/demofirebase/MainActivity.java
@@ -1,21 +1,31 @@
 package com.example.demofirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText edt_Email, edt_Password;
-    private Button btn_Login, btn_Singup;
+    private Button btn_Resert, btn_Singup;
+
+    TextView click_Login;
     private FirebaseAuth firebaseAuth;
 
     @Override
@@ -27,14 +37,23 @@ public class MainActivity extends AppCompatActivity {
 
         edt_Email = (EditText) findViewById(R.id.edt_email);
         edt_Password = (EditText) findViewById(R.id.edt_pass);
-        btn_Login = (Button) findViewById(R.id.btn_login);
+        btn_Resert = (Button) findViewById(R.id.btn_resert);
         btn_Singup = (Button) findViewById(R.id.btn_singup);
+        click_Login = (TextView) findViewById(R.id.click_login);
 
-        btn_Login.setOnClickListener(new View.OnClickListener() {
+        click_Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btn_Resert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edt_Email.setText("");
+                edt_Password.setText("");
             }
         });
 
@@ -54,10 +73,29 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
 
-                if (password.length() < 8){
-                    Toast.makeText(MainActivity.this, "Password phải trên 8 ký tự", Toast.LENGTH_SHORT).show();
+                if (password.length() < 6){
+                    Toast.makeText(MainActivity.this, "Password phải trên 6 ký tự", Toast.LENGTH_SHORT).show();
                     return;
                 }
+
+
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(MainActivity.this, "Tạo tài khoản thành công", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(MainActivity.this, "Tạo tài khoản thất bại",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
             }
         });
     }
